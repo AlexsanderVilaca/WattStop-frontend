1b287eb7f561c55e0606a47c5ffdbf60dfa1b8ac
Dravvos <daniddias53@gmail.com>
2023-11-25 21:05:42 -0300

    add: conteúdo página sobre

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -20,6 +20,17 @@
         </l-popup>
       </l-marker> -->
     </l-map>
+<br/><br/>
+<div class="d-flex flex-column text-center">
+    <h1>
+        Integrantes
+    </h1>
+    <p>
+        Alexsander Vilaça Rodrigues - 081200032<br />
+        Daniel Oliveira Dias - 081200022<br />
+        Marcus Augusto Casé Nutti - 081200027<br />
+    </p>
+  </div>
   </div>
 </template>
 
